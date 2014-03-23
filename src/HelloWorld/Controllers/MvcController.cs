76afb4e446d2aed8e4f7fe4315b32f427b797e33
Rob Moore <me@robdmoore.id.au>
2014-03-23 15:21:32 +0800

    Added source files to generate deployable site

---
+++ src/HelloWorld/Controllers/MvcController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class MvcController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/HelloWorld/Controllers/WebApiController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace HelloWorld.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        [Route("api/helloworld")]
+        public MessageResponse Get()
+        {
+            return new MessageResponse
+            {
+                Message = "Hello World!"
+            };
+        }
+    }
+
+    public class MessageResponse
+    {
+        public string Message { get; set; }
+    }
+
+}
